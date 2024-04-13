2196572c1888acfe79bbc7d22790874902d4f2d6
maria_koren <maria_koren@yahoo.com>
2024-04-13 22:07:21 +0200

    prepare

--- Hipotezy/h1.py
+++ Hipotezy/h1.py
@@ -7,8 +7,31 @@ from podstawowa import *
 from a1 import alg1
 from a2 import gauss_elimination_with_partial_pivoting
 from a3 import gauss_seidl
-matrix, vector = main()
+# matrix, vector = main()
 
+# matrix1= copy.deepcopy(matrix)
+# matrix2 = copy.deepcopy(matrix)
+# matrix3 = copy.deepcopy(matrix)
+
+# vector1 = copy.deepcopy(vector)
+# vector2 = copy.deepcopy(vector)
+# vector3 = copy.deepcopy(vector)
+
+# res1 = alg1(matrix1, vector1)
+# res2 = gauss_elimination_with_partial_pivoting(np.array(matrix2), np.array(list(map(lambda x: [x], vector2))))
+# res3 = gauss_seidl(np.array(matrix3), np.array(vector3))
+
+# print(res1)
+# print()
+# print(res2)
+# print()
+# print(res3)
+
+matrix = [[1.2,  2.6, -0.1,  1.5],
+           [4.5,  9.8, -0.4,  5.7],
+           [0.1, -0.1, -0.3, -3.5],
+           [4.5, -5.2,  4.2, -3.4]]
+vector = [13.15, 49.84, -14.08, 46.51]
 matrix1= copy.deepcopy(matrix)
 matrix2 = copy.deepcopy(matrix)
 matrix3 = copy.deepcopy(matrix)
@@ -19,10 +42,10 @@ vector3 = copy.deepcopy(vector)
 
 res1 = alg1(matrix1, vector1)
 res2 = gauss_elimination_with_partial_pivoting(np.array(matrix2), np.array(list(map(lambda x: [x], vector2))))
-res3 = gauss_seidl(np.array(matrix3), np.array(vector3))
+# res3 = gauss_seidl(np.array(matrix3), np.array(vector3))
 
 print(res1)
 print()
 print(res2)
 print()
-print(res3)
+# print(res3)
---
+++ montecarlo.py
@@ -0,0 +1,7 @@
+from podstawowa import *
+from a1 import alg1
+from a2 import gauss_elimination_with_partial_pivoting
+from a3 import gauss_seidl
+from podstawowa import *
+
+graf = 
